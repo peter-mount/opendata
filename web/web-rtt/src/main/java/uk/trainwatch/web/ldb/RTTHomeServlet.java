c4aa74471cb1808ce2905f359a77f7bb198664d8
peter <peter@europa.area51.onl>
2015-06-06 13:38:48 +0100

    Beginnings of the tracking module

---
+++ web/web-rtt/src/main/java/uk/trainwatch/web/ldb/RTTHomeServlet.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.trainwatch.web.ldb;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import uk.trainwatch.web.servlet.AbstractServlet;
+import uk.trainwatch.web.servlet.ApplicationRequest;
+
+/**
+ *
+ * @author peter
+ */
+@WebServlet(name = "RTTHomeServlet", urlPatterns = {"/rtt/", "/rtt"})
+public class RTTHomeServlet
+        extends AbstractServlet
+{
+
+    @Override
+    protected void doGet( ApplicationRequest request )
+            throws ServletException,
+                   IOException
+    {
+        // Set the date range from 7 days ago to now
+        Map<String, Object> req = request.getRequestScope();
+        LocalDate end = LocalDate.now();
+        LocalDate start = end.minus( 7, ChronoUnit.DAYS );
+        req.put( "start", start );
+        req.put( "end", end );
+
+        req.put( "time", LocalTime.now() );
+
+        request.renderTile( "rtt.home" );
+    }
+
+}
