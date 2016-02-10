c510f75f41080e104494c4617187383ea52b384f
peter <peter@europa>
2016-02-10 18:07:56 +0000

    Map builder

---
+++ core/src/main/java/uk/trainwatch/util/MapBuilder.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016 peter.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.trainwatch.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ *
+ * @author peter
+ */
+public interface MapBuilder<K, V>
+{
+
+    MapBuilder<K, V> add( K k, V v );
+
+    Map<K, V> build();
+
+    static <K, V> MapBuilder<K, V> builder()
+    {
+        return builder( new HashMap<>() );
+    }
+
+    static <K, V> MapBuilder<K, V> concurrentBuilder()
+    {
+        return builder( new ConcurrentHashMap<>() );
+    }
+
+    static <K, V> MapBuilder<K, V> builder( Map<K, V> map )
+    {
+        return new MapBuilder<K, V>()
+        {
+            @Override
+            public MapBuilder<K, V> add( K k, V v )
+            {
+                map.put( k, v );
+                return this;
+            }
+
+            @Override
+            public Map<K, V> build()
+            {
+                return map;
+            }
+        };
+    }
+
+}
